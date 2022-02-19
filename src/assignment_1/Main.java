e36d0af697072d676c2d6e9470a1dae4f8b6552e
Samual McCracken <mccracken.landon@gmail.com>
2022-02-19 15:37:02 -0500

    add set unit price of order

--- src/assignment_1/Main.java
+++ src/assignment_1/Main.java
@@ -3,7 +3,10 @@ package assignment_1;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*
+		 * Assignment 1 for UMGC
+		 * Creator: Samual McCracken
+		 */
 
 	}
 
--- src/assignment_1/Order.java
+++ src/assignment_1/Order.java
@@ -11,20 +11,39 @@ public class Order {
 	boolean setCustomerID(String user_input) {
 		try {
 			this.customer_ID = Integer.parseInt(user_input);
-			if(this.customer_ID < 0 || this.customer_ID > Integer.MAX_VALUE)
-			{
+			if(this.customer_ID < 0 || this.customer_ID > Integer.MAX_VALUE) {
 				System.err.printf("User input '%s' is out of bounds");
 				this.customer_ID = 0;
 				return false;
 			}
 		}
 		catch(NumberFormatException e) {
+			System.err.println(e);
 			System.err.printf("User input '%s' is not an integer type.\n", user_input);
 			this.customer_ID = 0;
 			return false;
 		}
 		return true;
 			
+	}
+	
+	boolean setUnitPrice(String user_input) {
+		if (user_input.startsWith("$")) {
+			user_input = user_input.replace("$", "");
+		}
+		try {
+			this.unit_price = Float.parseFloat(user_input);
+			if(this.unit_price < 0) {
+				System.err.println("Unit price may not be negative.");
+				this.unit_price = 0;
+				return false;
+				}
+		}
+		catch(NumberFormatException e) {
+			System.err.println(e);
+			System.err.printf("User input '%s' is not a float. (e.g. 12.45", user_input);
+		}
+		return false;
 	}
 
 }
