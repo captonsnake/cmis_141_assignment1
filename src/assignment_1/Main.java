429b63c2c5a474450605f67a405c261641091717
Landon McCracken <mccracken.landon@gmail.com>
2022-02-21 15:23:13 -0500

    add method reference proto

--- src/assignment_1/Main.java
+++ src/assignment_1/Main.java
@@ -1,13 +1,25 @@
 package assignment_1;
 
+import java.util.function.*;
+
 public class Main {
+	
+	public static void getInput(String prompt, Function<String, Boolean> setFunc) {
+		setFunc.apply(prompt);
+		return;
+	}
 
 	public static void main(String[] args) {
 		/*
 		 * Assignment 1 for UMGC
 		 * Creator: Samual McCracken
 		 */
-
+		Order order = new Order();
+		
+		while(true) {
+			// main loop
+				getInput("test", order::setDescription);
+				System.out.println(order.description);
+		}
 	}
-
 }
