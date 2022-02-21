921f143c3c93b5885cdc1d24eb36feb3157f6abb
Landon McCracken <mccracken.landon@gmail.com>
2022-02-21 15:44:20 -0500

    add some code

--- src/assignment_1/Main.java
+++ src/assignment_1/Main.java
@@ -1,11 +1,20 @@
 package assignment_1;
 
+import java.util.Scanner;
 import java.util.function.*;
 
 public class Main {
 	
 	public static void getInput(String prompt, Function<String, Boolean> setFunc) {
-		setFunc.apply(prompt);
+		Scanner scanner = new Scanner(System.in);
+		while(true) {
+			System.out.printf("%s", prompt);
+			String user_input = scanner.nextLine();
+			if(setFunc.apply(user_input)) {
+				break;
+			}
+			System.out.println("Invalid input, try again.");
+		}
 		return;
 	}
 
@@ -18,8 +27,9 @@ public class Main {
 		
 		while(true) {
 			// main loop
-				getInput("test", order::setDescription);
-				System.out.println(order.description);
+				getInput("Enter Cust ID: ", order::setCustomerID);
+				System.out.println(order.customer_ID);
+				break;
 		}
 	}
 }
